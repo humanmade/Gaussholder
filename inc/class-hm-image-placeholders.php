8b9649f295cc3c1d4ead1b3d14a125d72b88c20c
paul de wouters <paul@hmn.md>
2015-07-01 15:45:35 +0200

    Remove unnecessary methods

--- inc/class-hm-image-placeholders.php
+++ inc/class-hm-image-placeholders.php
@@ -36,17 +36,9 @@ class HM_Image_Placeholders {
 	 */
 	public function plugins_loaded() {
 
-		add_filter( 'wp_generate_attachment_metadata', array(
-			$this,
-			'filter_wp_generate_attachment_metadata'
-		), 10, 2 );
+		add_filter( 'wp_generate_attachment_metadata', array( $this, 'filter_wp_generate_attachment_metadata' ), 10, 2 );
 
-//add_filter( 'image_send_to_editor', array( $this, 'filter_image_send_to_editor' ), 10, 8 );
-
-		add_filter( 'wp_get_attachment_image_attributes', array(
-			$this,
-			'filter_wp_get_attachment_image_attributes'
-		), 10, 3 );
+		add_filter( 'wp_get_attachment_image_attributes', array( $this, 'filter_wp_get_attachment_image_attributes' ), 10, 3 );
 	}
 
 	function filter_wp_get_attachment_image_attributes( $attr, $attachment, $size ) {
@@ -112,6 +104,7 @@ class HM_Image_Placeholders {
 	}
 
 	/**
+	 * 
 	 * @param $id
 	 * @param $image_path
 	 */
@@ -143,57 +136,8 @@ class HM_Image_Placeholders {
 		}
 		$rgb = array( $r, $g, $b );
 
-//return implode(",", $rgb); // returns the rgb values separated by commas
+		//return implode(",", $rgb); // returns the rgb values separated by commas
 		return $rgb; // returns an array with the rgb values
 	}
 
-	/**
-	 * @param $html
-	 * @param $id
-	 *
-	 * @return string
-	 */
-	public function filter_image_send_to_editor( $html, $id, $caption, $title, $align, $url, $size, $alt ) {
-
-		$new_html = $this->generate_image_markup( $id );
-
-		if ( ! empty( $new_html ) ) {
-			return $new_html;
-		}
-
-		return $html;
-	}
-
-	public function generate_image_markup( $attachment_id ) {
-
-//fetching attachment by post $id
-		$attachment = get_post( $attachment_id );
-		$mime_type  = $attachment->post_mime_type;
-
-//get an valid array of images types
-		$image_exts = array( 'image/jpg', 'image/jpeg', 'image/jpe', 'image/gif', 'image/png' );
-
-//checking the above mime-type
-		if ( in_array( $mime_type, $image_exts ) ) {
-
-// the image link would be great
-			$src = wp_get_attachment_url( $attachment_id );
-
-			$colors_hex = get_post_meta( $attachment_id, 'hmgp_image_colors', true );
-
-			if ( ! $colors_hex ) {
-				return;
-			}
-
-			foreach ( $colors_hex as $hex ) {
-				$colors[] = implode( ',', $this->hex2rgb( $hex ) ) . ',0';
-				$colors[] = implode( ',', $this->hex2rgb( $hex ) ) . ',1';
-			}
-
-// enter you custom output here, you will want to probably change this
-			$html = '<a href="' . $src . '" class="your-class" data-src="/img/image1.jpg" rel="your-rel"><img style="background:linear-gradient(90deg, rgba(' . $colors[0] . ') 0%, rgba(' . $colors[1] . ') 100%, rgba(' . $colors[1] . ') 100%) , linear-gradient(0deg, rgba(' . $colors[2] . ') 0%, rgba(' . $colors[3] . ') 100%, rgba(' . $colors[3] . ') 100%) , linear-gradient(-90deg, rgba(' . $colors[4] . ') 0%, rgba(' . $colors[5] . ') 100%, rgba(' . $colors[5] . ') 100%) , linear-gradient(-180deg, rgba(' . $colors[6] . ') 0%, rgba(' . $colors[7] . ') 100%, rgba(' . $colors[7] . ') 100%);" src="' . $src . '" /></a>';
-
-			return $html; // return new $html
-		}
-	}
 }
