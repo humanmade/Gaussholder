7abfaa28c82c0c28a7c395f442e96871c99132e6
Ignacio Cruz <igmoweb@gmail.com>
2019-12-17 11:26:44 +0100

    Check for imagick before adding the admin_notice action for a better performance

--- inc/admin/namespace.php
+++ inc/admin/namespace.php
@@ -10,18 +10,18 @@ function bootstrap() {
 		return;
 	}
 
-	add_action( 'admin_notices', __NAMESPACE__ . '\\maybe_display_imagick_notice' );
+	if ( ! class_exists( 'Imagick' ) ) {
+		add_action( 'admin_notices', __NAMESPACE__ . '\\display_imagick_notice' );
+	}
 }
 
 /**
  * Display a notice when Imagick PHP extension is not available.
  */
-function maybe_display_imagick_notice() {
-	if ( ! class_exists( 'Imagick' ) ) {
-		?>
-		<div class="notice notice-error">
-			<p><?php _e( 'The Imagick PHP extension is not installed. Gaussholder cannot process images without it. Please, install and activate Imagick extension.', 'gaussholder' ); ?></p>
-		</div>
-		<?php
-	}
+function display_imagick_notice() {
+	?>
+	<div class="notice notice-error">
+		<p><?php _e( 'The Imagick PHP extension is not installed. Gaussholder cannot process images without it. Please, install and activate Imagick extension.', 'gaussholder' ); ?></p>
+	</div>
+	<?php
 }
