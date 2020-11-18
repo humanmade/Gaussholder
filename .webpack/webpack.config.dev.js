3a098a18cef31ca26b876b6b4cb87f053f25f21d
Ignacio Cruz <igmoweb@gmail.com>
2020-11-18 15:42:10 +0100

    Added ESLint loader in webpack

--- .webpack/webpack.config.dev.js
+++ .webpack/webpack.config.dev.js
@@ -4,4 +4,18 @@ module.exports = {
 	...common,
 	mode: 'development',
 	devtool: 'inline-source-map',
+	module: {
+		...common.module,
+		rules: [
+			...common.module.rules,
+			{
+				test: /\.js?$/,
+				exclude: /(node_modules|bower_components)/,
+				enforce: 'pre',
+				loader: require.resolve( 'eslint-loader' ),
+				options: {},
+			},
+		]
+
+	}
 };
