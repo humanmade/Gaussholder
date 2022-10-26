5ea440cb3c76224803244e508455717782277828
robindevitt <robin@humanmade.com>
2022-10-26 13:49:47 +0200

    Updated comment and docs.

--- assets/index.js
+++ assets/index.js
@@ -2,4 +2,5 @@ import Gaussholder from './src/gaussholder';
 
 document.addEventListener( 'DOMContentLoaded', Gaussholder );
 
+// Add the Gaussholder function to the window object.
 window.GaussHolder = Gaussholder;
